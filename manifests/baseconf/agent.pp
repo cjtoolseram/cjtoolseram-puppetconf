27e57f586fddfd9628db28854b8bd0803a78daa5
Rob Burghardt <rburghardt1@udayton.edu>
2019-06-24 16:16:03 -0400

    add support for global $environment variable.

--- manifests/baseconf/agent.pp
+++ manifests/baseconf/agent.pp
@@ -3,7 +3,15 @@ class puppetconf::baseconf::agent ($master = undef,
   $conf_path = '/etc/puppetlabs/puppet/puppet.conf',
   $caserver = undef,
   $runinterval = '30m',
+  $environment = 'production',
   ){
+
+  if $::environment != undef {
+    $local_environment = $::environment
+  }
+  else {
+    $local_environment = $environment
+  }
 
   #the define types defaults
   Puppetconf::Main {
@@ -22,9 +30,13 @@ class puppetconf::baseconf::agent ($master = undef,
   puppetconf::main { 'ca_server':
     value     => $caserver,
   }
-  
+
   puppetconf::main { 'runinterval':
     value     => $runinterval,
+  }
+
+  puppetconf::main { 'environment':
+    value     => $local_environment,
   }
 
   ## section agent config
